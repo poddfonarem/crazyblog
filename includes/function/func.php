e045a14a01e9e003d5c32d5e367b9790dad7cc8a
PODDFONAREM <91065141+poddfonarem@users.noreply.github.com>
2025-04-17 23:20:08 +0300

    Update function file
    Update function after DAST test (wapiti)

--- includes/function/func.php
+++ includes/function/func.php
@@ -1,4 +1,11 @@
 <?php
+header("X-Content-Type-Options: nosniff; Content-Security-Policy: frame-ancestors 'none'; 
+Content-Security-Policy: default-src 'self'; script-src 'self'; style-src 'self';");
+
+session_set_cookie_params([
+    'secure' => true,
+    'httponly' => true,
+]);
 session_start();
 
 require_once __DIR__ . "/../../vendor/autoload.php";
